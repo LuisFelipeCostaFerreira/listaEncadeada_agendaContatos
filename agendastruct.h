59feac3224f0a924b47678121e90bb0b9d5a622e
LuisFelipeCostaFerreira <143102001+LuisFelipeCostaFerreira@users.noreply.github.com>
2023-09-27 10:34:44 -0300

    Update agendastruct.h

--- agendastruct.h
+++ agendastruct.h
@@ -27,35 +27,6 @@ struct telefone{
     struct telefone fone[2];
     struct contato *proximo;
 } Contato;
-/*
-Contato novoContato(){  
-    Contato novaPessoa;    
-
-    printf("Nome:");
-    scanf(" %50[^\n]", novaPessoa.nome);
-    printf("Email:");
-    scanf(" %30[^\n]", novaPessoa.email);
-    printf("Rua:");
-    scanf(" %30[^\n]", novaPessoa.end.rua);
-    printf("Numero da casa:");
-    scanf(" %d",&novaPessoa.end.numero);
-    printf("Bairro:");
-    scanf(" %50[^\n]", novaPessoa.end.bairro);
-    printf("Cidade:");
-    scanf(" %50[^\n]", novaPessoa.end.cidade);
-    printf("DDD(telefone fixo):");
-    scanf(" %s", novaPessoa.fone[0].DDD);
-    printf("Numero do telefone fixo:");
-    scanf(" %s", novaPessoa.fone[0].numero_telefone);
-    printf("DDD(telefone movel):");
-    scanf(" %s", novaPessoa.fone[1].DDD);
-    printf("Numero do telefone movel:");
-    scanf(" %s", novaPessoa.fone[1].numero_telefone);
-    
-
-    return novaPessoa; 
-}
-*/
 
  #endif
 
